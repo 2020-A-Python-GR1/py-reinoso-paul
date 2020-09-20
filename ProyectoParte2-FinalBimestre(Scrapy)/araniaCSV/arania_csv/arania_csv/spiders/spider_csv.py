8052712e54f2fab51eac56fbdd4c001fdb287b5c
Paul Reinoso <washington.reinoso@epn.edu.ec>
2020-09-20 15:41:28 -0500

    Proyecto-segundaparte- II-spider-csv-dataset-creado

--- ProyectoParte2-FinalBimestre(Scrapy)/araniaCSV/arania_csv/arania_csv/spiders/spider_csv.py
+++ ProyectoParte2-FinalBimestre(Scrapy)/araniaCSV/arania_csv/arania_csv/spiders/spider_csv.py
@@ -8,10 +8,10 @@ class SerieSpider(CrawlSpider):
     name = 'arania_juego_csv'
 
     start_urls = [
-        'https://www.nintenderos.com/juegos/'
+        'https://www.nintenderos.com/juegos/'    
     ]
 
-    for i in range(1, 20):
+    for i in range(1, 29):
         start_urls.append(
             'https://www.nintenderos.com/juegos/page/{i}/'.format(i=i))
 
@@ -42,34 +42,104 @@ class SerieSpider(CrawlSpider):
     )
 
     rules = regla_uno
-    name_serie = []
+    name_serie = []    
+    desarrolladores=list()
+    distribuidor=list()
+    editor=list()
+    generos=list()
+    plataforma=list()
+    precio_lanzamiento=list()
+    consola_virtual=list()
+    lanzamiento_europa=list()
+    formato =list()
+    voces_texto=list()
+    textos_idioma=list()
+    jugadores=list()
+    modo_online=list()
+    num_idiomas=list()
+    num_generos=list()
+    nombre=list()
+    puntos=list()
+    edad_rec=list()
 
     def parse_page(self, response):
 
-        # response.css('h1.c-post-single__title ::text').extract() obtienen nombre del juego
-        # response.css('div.top ::text').extract() obtienen puntaje del juego
-            
-        name_serie = response.css(
-            'div.c-post-single__container > ul.caracteristicas ::text').extract()[2]
-        self.name_serie.append(name_serie)
-        #info_uno=response.css('div.col-md-6 > ul.li-inl ::text').extract()
-        #info_dos=response.css('div.mod-li > ul.li-inl ::text').extract()
-        # print(info_dos)
-        # print(info_uno)
-        # for nb in nombre:
-        #    with open('nombres_juegos.txt','a+',encoding='utf-8') as archivo:
-        #        archivo.write(nb+'\n')
+        # consultas
+        nombre_juego=response.css('h1.c-post-single__title ::text').extract()[0] #obtienen nombre del juego
+        puntuacion_juego=response.css('div.top ::text').extract()[3]# obtienen puntaje del juego
+        edad_recomendada=response.css('div.pegi-image img::attr(alt)').extract()[0] #edad recomendada            
+        name_serie = response.css('div.c-post-single__container > ul.caracteristicas ::text').extract() #toda la informacion general
+        #limpiamos        
+        completo=list()
+        for i in name_serie:
+            completo.append(i.strip("\n\t\t\t\t\t\t").strip("\n\t\t").strip("\t\t\t\t\t\t").strip(",").strip(" ").strip("\n"))        
+        completo = list(filter(None, completo))
+        
+        #Formato para generos/unimos los generos en un solo campo
+        generos=list()        
+        indices_genero=list()
+        cont=0
+        for i in range((completo.index("Géneros:")+1),len(completo)):
+            if(i<completo.index("Plataforma:")):
+                generos.append(completo[i])
+                cont=cont+1
+                indices_genero.append(i)
+        
+        for x in range(0,len(indices_genero)):
+            completo.pop(indices_genero[x])
+
+        gener='-'.join(generos)        
+        completo.insert((completo.index("Géneros:")+1),gener)
+        print("GENEROS",cont)
+                        
+        #formato para el texto de idioma
+        for i in range((completo.index("Textos (idioma):")+1),len(completo)):
+            if(i<completo.index("Jugadores:")):
+                num_idiomas = len(completo[i].split(','))
+                idiom=completo[i].replace(', ', '-')                
+                completo.pop(i)                
+        print("IDIOMAS",num_idiomas)        
+        completo.insert((completo.index("Textos (idioma):")+1),idiom)        
+                
+        self.nombre.append(nombre_juego)        
+        self.puntos.append(puntuacion_juego)
+        self.edad_rec.append(edad_recomendada)
+        self.desarrolladores.append(completo[(completo.index('Desarrollador:')+1)])        
+        self.generos.append(completo[(completo.index('Géneros:')+1)])
+        self.plataforma.append(completo[(completo.index('Plataforma:')+1)])
+        self.precio_lanzamiento.append(completo[(completo.index('Precio de lanzamiento:')+1)])
+        self.consola_virtual.append(completo[(completo.index('Consola virtual:')+1)])
+        self.lanzamiento_europa.append(completo[(completo.index('Lanzamiento en Europa:')+1)])
+        self.formato.append(completo[(completo.index('Formato:')+1)])
+        self.voces_texto.append(completo[(completo.index('Voces (idioma):')+1)])
+        self.textos_idioma.append(completo[(completo.index('Textos (idioma):')+1)])
+        self.jugadores.append(completo[(completo.index('Jugadores:')+1)])
+        self.modo_online.append(completo[(completo.index('Modo Online:')+1)])        
+        self.distribuidor.append(completo[(completo.index('Distribuidor:')+1)])
+        self.num_generos.append(cont)
+        self.num_idiomas.append(num_idiomas)
 
     def closed(self, reason):
         save_path = '../series.csv'
         df = pd.DataFrame({
-            'name_serie': pd.Series(self.name_serie),
-            #'release_date': pd.Series(self.release_date),
-            #'runtime': pd.Series(self.runtime),
-            #'end_date': pd.Series(self.end_date),
-            #'genre': pd.Series(self.genre),
-            #'rating': pd.Series(self.rating),
-            #'votes': pd.Series(self.rating_votes)
+            
+            'Nomre': pd.Series(self.nombre),
+            'Puntuacion': pd.Series(self.puntos),
+            'Clasificacion Edad': pd.Series(self.edad_rec),
+            'Desarrollador': pd.Series(self.desarrolladores),
+            'Distribuidor': pd.Series(self.distribuidor),
+            'Géneros': pd.Series(self.generos),
+            'Plataforma': pd.Series(self.plataforma),
+            'Precio': pd.Series(self.precio_lanzamiento),
+            'Consola virtual': pd.Series(self.consola_virtual),
+            'Lanzamiento en Europa:': pd.Series(self.lanzamiento_europa),
+            'Formato': pd.Series(self.formato),
+            'Voces (idioma)': pd.Series(self.voces_texto),
+            'Textos (idioma)': pd.Series(self.textos_idioma),
+            'Num Jugadores': pd.Series(self.jugadores),
+            'Modo Online': pd.Series(self.modo_online),
+            'Num genero': pd.Series(self.num_generos),
+            'Num idioma': pd.Series(self.num_idiomas),
         }
         )
         df.to_csv(save_path, index=False)
